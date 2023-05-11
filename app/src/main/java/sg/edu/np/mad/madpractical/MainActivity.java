ee488128cdf866f9899b5cb06f547ff6780d8a19
BraydenWu <s10221128@connect.np.edu.sg>
2023-05-11 10:51:50 +0800

    Practical 2 Done

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -3,6 +3,12 @@ package sg.edu.np.mad.madpractical;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button followButton = findViewById(R.id.button);
+        //ToggleButton Follow = (ToggleButton)findViewById(R.id.button);
+        User user = new User("John Doe", "Software Engineer", true);
+
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (user.isFollowed()) {
+                    user.setFollowed(false);
+                    followButton.setText("Follow");
+                } else {
+                    user.setFollowed(true);
+                    followButton.setText("Unfollow");
+                }
+            }
+        });
     }
 
 }
+
+
+
--- app/src/main/java/sg/edu/np/mad/madpractical/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -1,4 +1,29 @@
 package sg.edu.np.mad.madpractical;
-public class User {
 
+public class User {
+    private String name;
+    private String description;
+    private boolean followed;
+
+    public User(String name, String description, boolean followed) {
+        this.name = name;
+        this.description = description;
+        this.followed = followed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isFollowed() {
+        return followed;
+    }
+
+    public void setFollowed(boolean followed) {
+        this.followed = followed;
+    }
 }
